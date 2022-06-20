41d15858e4a9bdc47045e3def2d5f7a7f9f40435
Franziska-F <f.fouchs@gmail.com>
2022-06-20 23:51:19 +0200

    API is working, add display of title, author and cover

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,14 @@ import { useState } from 'react';
 
 export default function Home() {
   const [result, setResult] = useState([]);
-
+  const [bookTitle, setBookTitle] = useState('');
   function handleSearch() {
-    fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=intitle:der%20fremd+inauthor:camus`,
-    )
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${bookTitle}`)
       .then((res) =>
         res.json().then((response) => {
-          setResult(response);
-          console.log(response);
+          const bookInfo = response.items[0].volumeInfo;
+          console.log(response.items[0].volumeInfo);
+          setResult(bookInfo);
         }),
       )
       .catch(() => {
@@ -22,13 +21,34 @@ export default function Home() {
     <div>
       <h1>Read, write, connect</h1>
       <div>
-        <input placeholder="Search for a book" />
+        <input
+          placeholder="Search for a book"
+          onChange={(event) => {
+            const title = event.currentTarget.value;
+
+            setBookTitle(title);
+          }}
+        />
         <button onClick={handleSearch}>Search</button>
       </div>
       <div>
+        {' '}
+        <p>{result.title}</p>
+        <p>{result.authors}</p>
+        <img
+          src={result.imageLinks ? result.imageLinks.thumbnail : '/'}
+          alt={result.imageLinks ? 'bookcover' : 'No cover'}
+        />
+      </div>
+      <br />
+      <br />
+      <br />
+      <div>
         <button>Register</button>
       </div>
-      <button>Log in</button>
+      <div>
+        <button>Log in</button>
+      </div>
       {/* }
       https://www.googleapis.com/books/v1/volumes?q=intitle:der%20fremd+inauthor:camus {*/}
     </div>
